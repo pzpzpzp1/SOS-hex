1b88396072e2373b81874818d20845b257a231b9
pzpzpzp1 <pzpzpzp1@mit.edu>
2021-06-08 15:52:12 -0400

    add alternative degenerate hex formulation. find nullspace in grad_u(x(u))

---
+++ SOS_core/SOS_jacobian_single_hex_mod.m
@@ -0,0 +1,78 @@
+%{
+Computes the minimum eigenvalue of (gradX^T gradX) 
+	V			the vertices of the hex
+	k			optional, the degree of the SOS relaxation (default 4)
+returns:
+	--- tbd
+%}
+function [J, argmin, second_eig, diagnostics] = SOS_jacobian_single_hex_mod(V, k)
+    if nargin == 0;
+        V = rand_hex(1,.3);
+    end
+    
+	if nargin < 2; k = 4; end
+
+	sdpvar u v w bound v1 v2 v3;
+    % syms u v w real;
+
+	% set up model
+	vars1 = [u v w];
+    vars2 = [v1 v2 v3];
+    vars = [u, v, w, v1, v2, v3];
+%     vars = [u, v, w];
+	Dphi = symbolic_trilinear_map_jacobian(V, [u,v,w]);
+    Dphiv = Dphi*vars(4:6)'; % Dphi*v == 0
+    vnorm_g = 1 - (v1^2+v2^2+v3^2); % |v|^2 <= 1
+    d = randn(3,1); d = d/norm(d);
+    vobj = dot([v1 v2 v3], d); % maximize v1
+%     J = dot(cross(Dphi(:,1),Dphi(:,2)),Dphi(:,3));
+%     J = symbolic_jacobian_det(V, [u, v, w]);
+
+	[s1, s1_c] = polynomial(vars, k);
+	[s2, s2_c] = polynomial(vars, k);
+	[s3, s3_c] = polynomial(vars, k);
+	[s4, s4_c] = polynomial(vars, k);
+	[s5, s5_c] = polynomial(vars, k);
+	[s6, s6_c] = polynomial(vars, k);
+    [s7, s7_c] = polynomial(vars, k);
+% 	[s8, s8_c] = polynomial(vars2, k);
+% 	[s9, s9_c] = polynomial(vars2, k);
+% 	[s10, s10_c] = polynomial(vars2, k);
+% 	[s11, s11_c] = polynomial(vars2, k);
+% 	[s12, s12_c] = polynomial(vars2, k);
+    [r1, r1_c] = polynomial(vars, k);
+    [r2, r2_c] = polynomial(vars, k);
+    [r3, r3_c] = polynomial(vars, k);
+
+	domain = [u; 1 - u; v; 1 - v; w; 1 - w; ... % u in 0-1 cube
+        vnorm_g;];
+%            1-v1; 1+v1; 1-v2; 1+v2; 1-v3; 1+v3;]; % v1,v2,v3 in [-1,1] cube
+%     domain = [u; 1 - u; v; 1 - v; w; 1 - w; ] % u in 0-1 cube
+    
+	F_sos = [sos(s1), sos(s2), sos(s3), sos(s4), sos(s5), sos(s6),...
+       sos(s7)]; %, sos(s8), sos(s9), sos(s10), sos(s11), sos(s12)];
+%     F_sos = [sos(s1), sos(s2), sos(s3), sos(s4), sos(s5), sos(s6)];
+% 	F_positiv = sos(vobj - bound - [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12]*domain - [r1,r2,r3]*Dphiv);
+    F_positiv = sos(vobj - bound - [s1, s2, s3, s4, s5, s6, s7]*domain - [r1,r2,r3]*Dphiv);
+%     F_positiv = sos(J - bound - [s1, s2, s3, s4, s5, s6]*domain);
+
+	[F, obj, m] = sosmodel([F_sos, F_positiv], -bound, [], [s1_c; s2_c; s3_c; s4_c; s5_c; s6_c; bound;...
+    s7_c; r1_c; r2_c; r3_c;]);    
+    % s7_c; s8_c; s9_c; s10_c; s11_c; s12_c; r1_c; r2_c; r3_c;]);
+%     [F, obj, m] = sosmodel([F_sos, F_positiv], -bound, [], [s1_c; s2_c; s3_c; s4_c; s5_c; s6_c; bound;]);
+	
+	% solve the problem
+	diagnostics = optimize(F, obj, []);
+
+	% extract the answers
+	J = value(bound);
+
+	moment_mat = dual(F(8));
+	moment_mat = moment_mat/moment_mat(1,1);
+
+	eig_vals = eig(moment_mat);
+	second_eig = eig_vals(2);
+
+	point = moment_mat(2:4, 1);
+	argmin = point(end:-1:1);
+end
---
+++ SOS_core/symbolic_trilinear_map_jacobian.m
@@ -0,0 +1,59 @@
+%{
+The trilinear map for the hex with given vertices, evaluated at the given points
+	V	vertices of hex, 8x3
+	p	points to evaluate at, Nx3
+returns:
+	Dphi jacobian values of the trilinear map, 3x3xN
+%}
+function Dphi = symbolic_trilinear_map_jacobian(V, p)
+    u = reshape(p(:,1),1,1,[]);
+    v = reshape(p(:,2),1,1,[]);
+    w = reshape(p(:,3),1,1,[]);
+    V11 = V(1,1); V12 = V(1,2); V13 = V(1,3);
+    V21 = V(2,1); V22 = V(2,2); V23 = V(2,3);
+    V31 = V(3,1); V32 = V(3,2); V33 = V(3,3);
+    V41 = V(4,1); V42 = V(4,2); V43 = V(4,3);
+    V51 = V(5,1); V52 = V(5,2); V53 = V(5,3);
+    V61 = V(6,1); V62 = V(6,2); V63 = V(6,3);
+    V71 = V(7,1); V72 = V(7,2); V73 = V(7,3);
+    V81 = V(8,1); V82 = V(8,2); V83 = V(8,3);
+    
+	Dphi = [[V41.*v.*(w - 1) - V31.*v.*(w - 1) + V51.*w.*(v - 1) - V61.*w.*(v - 1) - V11.*(v - 1).*(w - 1) + V21.*(v - 1).*(w - 1) + V71.*v.*w - V81.*v.*w, V21.*u.*(w - 1) - V31.*u.*(w - 1) + V51.*w.*(u - 1) - V81.*w.*(u - 1) - V11.*(u - 1).*(w - 1) + V41.*(u - 1).*(w - 1) - V61.*u.*w + V71.*u.*w, V51.*(u - 1).*(v - 1) - V11.*(u - 1).*(v - 1) - V31.*u.*v + V71.*u.*v + V21.*u.*(v - 1) + V41.*v.*(u - 1) - V61.*u.*(v - 1) - V81.*v.*(u - 1)];...
+        [V42.*v.*(w - 1) - V32.*v.*(w - 1) + V52.*w.*(v - 1) - V62.*w.*(v - 1) - V12.*(v - 1).*(w - 1) + V22.*(v - 1).*(w - 1) + V72.*v.*w - V82.*v.*w, V22.*u.*(w - 1) - V32.*u.*(w - 1) + V52.*w.*(u - 1) - V82.*w.*(u - 1) - V12.*(u - 1).*(w - 1) + V42.*(u - 1).*(w - 1) - V62.*u.*w + V72.*u.*w, V52.*(u - 1).*(v - 1) - V12.*(u - 1).*(v - 1) - V32.*u.*v + V72.*u.*v + V22.*u.*(v - 1) + V42.*v.*(u - 1) - V62.*u.*(v - 1) - V82.*v.*(u - 1)];...
+        [V43.*v.*(w - 1) - V33.*v.*(w - 1) + V53.*w.*(v - 1) - V63.*w.*(v - 1) - V13.*(v - 1).*(w - 1) + V23.*(v - 1).*(w - 1) + V73.*v.*w - V83.*v.*w, V23.*u.*(w - 1) - V33.*u.*(w - 1) + V53.*w.*(u - 1) - V83.*w.*(u - 1) - V13.*(u - 1).*(w - 1) + V43.*(u - 1).*(w - 1) - V63.*u.*w + V73.*u.*w, V53.*(u - 1).*(v - 1) - V13.*(u - 1).*(v - 1) - V33.*u.*v + V73.*u.*v + V23.*u.*(v - 1) + V43.*v.*(u - 1) - V63.*u.*(v - 1) - V83.*v.*(u - 1)]];
+    
+end
+
+%{
+syms u v w real;
+syms V11 V12 V13 real;
+syms V21 V22 V23 real;
+syms V31 V32 V33 real;
+syms V41 V42 V43 real;
+syms V51 V52 V53 real;
+syms V61 V62 V63 real;
+syms V71 V72 V73 real;
+syms V81 V82 V83 real;
+
+symp = [u v w];
+symV = [V11 V12 V13;...
+V21 V22 V23 ;...
+V31 V32 V33 ;...
+V41 V42 V43 ;...
+V51 V52 V53 ;...
+V61 V62 V63 ;...
+V71 V72 V73 ;...
+V81 V82 V83 ;];
+phi = symbolic_trilinear_map(V, p);
+J11 = diff(phi(1),u);
+J12 = diff(phi(1),v);
+J13 = diff(phi(1),w);
+J21 = diff(phi(2),u);
+J22 = diff(phi(2),v);
+J23 = diff(phi(2),w);
+J31 = diff(phi(3),u);
+J32 = diff(phi(3),v);
+J33 = diff(phi(3),w);
+Jsym = [J11 J12 J13; J21 J22 J23; J31 J32 J33;];
+%}
+
